eb8ba58a4340f866b5c2322fd64852327ece1666
JCGG-99977 <1848514604@qq.com>
2021-11-16 17:12:14 +0800

    邮箱初步验证完成

--- router/sendemail.js
+++ router/sendemail.js
@@ -20,14 +20,23 @@ router.get('/email', (req, res) => {
         }
         return code
     }
+    var code=randonemail()
     //3.配置发送邮件的信息
     let mailOptions = {
         from: 'socialnorms@vip.qq.com', // 发送者
         to: req.query.email, // 传过来的邮箱
         subject: '注册验证', // 邮件标题
-        html: randonemail()
+        html: '您的验证码为：<b>'+code+'</b>,请妥善保管！'
     };
-
+     // 对字符串进行加密
+    function compileStr(code) {
+        // 进行字符的转换
+        var c = String.fromCharCode(code.charCodeAt(0) + code.length)
+        for (var i = 1; i < code.length; i++) {
+          c += String.fromCharCode(code.charCodeAt(i) + code.charCodeAt(i - 1))
+        }
+        return escape(c)
+      }
     //4.发送邮件
     transporter.sendMail(mailOptions, function (err, data) {
         //回调函数，用于判断邮件是否发送成功
@@ -37,7 +46,8 @@ router.get('/email', (req, res) => {
             console.log('发送成功')
             let data = {
                 code: 200,
-                msg: 'success'
+                msg: 'success',
+                emailcode:compileStr(code)
             }
             res.send(data)
         }
